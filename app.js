deecfc7326348aaf1d6e6c89b75542e1d24d389a
AndBru <andrisbrutans@outlook.com>
2021-02-20 02:34:11 +0100

    fixed the route i tried multiple times

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 
-app.use(express.static(__dirname + 'views'))
+app.use(express.static(__dirname + 'public'))
 
 
 items = ["item 1", "item2", "item3"]
@@ -35,7 +35,7 @@ app.post("/", function (req, res) {
   res.redirect('/');
 });
 
-app.listen(3000, function () {
-  console.log(`Example app listening at http://localhost:3000`)
+app.listen(3330, function () {
+  console.log(`Example app listening at http://localhost:3330`)
 
 });
