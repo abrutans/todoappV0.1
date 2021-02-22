e2f2554cfb5f27447a8d1a6f66387d71348ac03c
AndBru <andrisbrutans@outlook.com>
2021-02-22 01:30:59 +0100

    git ignore

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const ejs = require('ejs')
 
 app.use(express.static(__dirname + '/public'))
 
-items = ["Item 1", "Item 2", ""]
+items = []
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
+// TODO: create a method to remove a item once cbox is triggered.
 
 app.get("/", function (req, res) {
   let today = new Date()
@@ -20,7 +19,7 @@ app.get("/", function (req, res) {
     month: 'long'
   };
   day = today.toLocaleDateString("en-US", options)
-
+  
 
 
   res.render("list", { day: day, newListItems: items });
@@ -30,7 +29,11 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
   let item = req.body.newItem
+  let checkbox = req.body.cbox 
+
   items.push(item)
+  
+  
   res.redirect('/');
 });
 
